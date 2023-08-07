b4550177ca0558095fc2cecbea76b752007fe5ca
lijunlin <xiafeng.li@foxmail.com>
2023-08-07 09:59:14 +0800

    format

--- src/Utilities/PublicHeader/String.cpp
+++ src/Utilities/PublicHeader/String.cpp
@@ -283,8 +283,7 @@ bool FoundFirstNumberWithoutBrackets(const std::string &input, int *start,
 }
 
 std::string GenerateCudaVisiableDeviceStr(const crane::grpc::DedicatedResource& value){
-  uint64_t nvidia_graphics_card = value.nvidia_graphics_card();
-  int num = std::popcount(nvidia_graphics_card);
+  int num = std::popcount(value.nvidia_graphics_card());
   std::string retVal;
   retVal.reserve(num*2+1);
   for(int i = 0;i<num;++i){
