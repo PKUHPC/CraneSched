df8dee73783beeadc9c117bb7864d7ef22346dca
RileyW <wrllrwwrllrw@gmail.com>
2023-08-09 17:50:44 +0800

    Refactor code.
    Signed-off-by: RileyW <wrllrwwrllrw@gmail.com>

--- src/CraneCtld/DbClient.cpp
+++ src/CraneCtld/DbClient.cpp
@@ -45,8 +45,7 @@ bool MongodbClient::CheckDefaultRootAccountUserAndInit_() {
         std::remove_reference<decltype(qos.max_jobs_per_user)>::type>::max();
     qos.max_running_tasks_per_user = std::numeric_limits<std::remove_reference<
         decltype(qos.max_running_tasks_per_user)>::type>::max();
-    qos.max_time_limit_per_task =
-        absl::Seconds(google::protobuf::util::TimeUtil::kDurationMaxSeconds);
+    qos.max_time_limit_per_task = absl::Seconds(kMaxTimeLimitSecond);
     qos.max_cpus_per_user = std::numeric_limits<
         std::remove_reference<decltype(qos.max_cpus_per_user)>::type>::max();
     qos.max_cpus_per_account = std::numeric_limits<
