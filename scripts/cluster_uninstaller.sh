89ed7bf29b66c6937fe200aa455d46ae170730eb
tksky1 <63036400+tksky1@users.noreply.github.com>
2024-02-26 16:45:04 +0800

    feat: add ubuntu cluster_installer.sh

--- scripts/cluster_uninstaller.sh
+++ scripts/cluster_uninstaller.sh
@@ -36,23 +36,3 @@ pdsh "$craned_pdsh_params" -R ssh "sudo yum remove cranesched-craned -y" || erro
 
 pdcp "$db_pdsh_params" wipe_data.sh /tmp/
 pdsh "$db_pdsh_params" -R ssh "sudo bash /tmp/wipe_data.sh 5" || error_exit "wipe data fail"
-
-config="
-systemLog:
-  destination: file
-  logAppend: true
-  path: /var/log/mongodb/mongod.log
-storage:
-  dbPath: /var/lib/mongo
-  journal:
-    enabled: true
-processManagement:
-  fork: true
-  pidFilePath: /var/run/mongodb/mongod.pid
-  timeZoneInfo: /usr/share/zoneinfo
-net:
-  port: 27017
-  bindIp: 127.0.0.1
-"
-pdsh "$db_pdsh_params" -R ssh "echo "$config" > /etc/mongod.conf" || error_exit "uninstall mongodb fail"
-pdsh "$db_pdsh_params" -R ssh "sudo systemctl restart mongod"
