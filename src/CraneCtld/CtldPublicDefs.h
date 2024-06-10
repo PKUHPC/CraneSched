f5be06f7aa425dc4d723b22aa9261fd13acdcacd
NamelessOIer <namelessoier@protonmail.com>
2024-06-10 10:15:23 +0800

    feat: hold & release

--- src/CraneCtld/CtldPublicDefs.h
+++ src/CraneCtld/CtldPublicDefs.h
@@ -266,6 +266,7 @@ struct TaskInCtld {
 
   bool requeue_if_failed{false};
   bool get_user_env{false};
+  bool held{false};
 
   std::string cmd_line;
   std::unordered_map<std::string, std::string> env;
@@ -464,6 +465,7 @@ struct TaskInCtld {
     qos = val.qos();
 
     get_user_env = val.get_user_env();
+    held = val.held();
   }
 
   void SetFieldsByRuntimeAttr(crane::grpc::RuntimeAttrOfTask const& val) {
