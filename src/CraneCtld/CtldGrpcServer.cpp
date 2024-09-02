12f199201f651483b2a439dab4a719e4fc7a1c2a
huerni <1547156325@qq.com>
2024-09-02 15:58:25 +0800

    feat: Standardize item in proto and rename it to modifyField

--- src/CraneCtld/CtldGrpcServer.cpp
+++ src/CraneCtld/CtldGrpcServer.cpp
@@ -440,7 +440,7 @@ grpc::Status CraneCtldServiceImpl::ModifyEntity(
       return grpc::Status::OK;
     }
     modify_res = g_account_manager->ModifyAccount(
-        request->type(), request->name(), request->item(), request->value(),
+        request->type(), request->name(), request->modify_field(), request->value(),
         request->force());
     break;
   case crane::grpc::User: {
@@ -463,7 +463,7 @@ grpc::Status CraneCtldServiceImpl::ModifyEntity(
           "greater permissions as yourself");
       return grpc::Status::OK;
     }
-    if (request->item() == "admin_level") {
+    if (request->modify_field() == crane::grpc::ModifyEntityRequest_ModifyField_AdminLevel) {
       User::AdminLevel new_level;
       if (request->value() == "none") {
         new_level = User::None;
@@ -489,7 +489,7 @@ grpc::Status CraneCtldServiceImpl::ModifyEntity(
 
     modify_res = g_account_manager->ModifyUser(
         request->type(), request->name(), request->partition(),
-        request->account(), request->item(), request->value(),
+        request->account(), request->modify_field(), request->value(),
         request->force());
     break;
 
@@ -502,7 +502,7 @@ grpc::Status CraneCtldServiceImpl::ModifyEntity(
       return grpc::Status::OK;
     }
 
-    modify_res = g_account_manager->ModifyQos(request->name(), request->item(),
+    modify_res = g_account_manager->ModifyQos(request->name(), request->modify_field(),
                                               request->value());
   } break;
 
