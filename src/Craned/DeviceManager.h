c4d3dabe0a5fc6fd46d365dea7cde0438d4a28a2
wtr <wangtaoran0504nb@outlook.com>
2024-10-15 10:57:36 +0800

    complete devices support in cgroupV2 by using bpf
    complete devices support in cgroupV2 by using bpf
    
    complete devices support in cgroupV2 by using bpf

--- src/Craned/DeviceManager.h
+++ src/Craned/DeviceManager.h
@@ -21,6 +21,19 @@
 
 namespace Craned {
 
+enum BPF_PERMISSION{
+    ALLOW = 0,
+    DENY
+};
+
+struct BpfDeviceMeta {
+    unsigned int major;
+    unsigned int minor;
+    int permission;
+    short access;
+    short type;
+};
+
 struct BasicDevice {
   std::string dev_id;
 
