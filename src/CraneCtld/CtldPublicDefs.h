35f0ead5ac9a5a10dc0212d8875e18b9bab60b8f
huerni <47264950+huerni@users.noreply.github.com>
2024-10-15 16:55:49 +0800

    feat: external tls

--- src/CraneCtld/CtldPublicDefs.h
+++ src/CraneCtld/CtldPublicDefs.h
@@ -88,7 +88,11 @@ struct Config {
     std::string CraneCtldListenPort;
 
     bool UseTls{false};
-    TlsCertificates Certs;
+    TlsCertificates ExternalCerts;
+    TlsCertificates InternalCerts;
+    TlsCertificates CforedCerts;
+    std::string InternalCaContent;
+    std::string DomainSuffix;
   };
   CraneCtldListenConf ListenConf;
 
