b62557a317ebeb3f83456cb10a0c6dee7bfe9877
db <1301189887@qq.com>
2024-10-28 17:32:17 +0800

    modify Log modular Output

--- src/Utilities/PublicHeader/Logger.cpp
+++ src/Utilities/PublicHeader/Logger.cpp
@@ -20,22 +20,34 @@
 
 void InitLogger(spdlog::level::level_enum level,
                 const std::string& log_file_path) {
-  spdlog::set_pattern("[%^%L%$ %C-%m-%d %s:%#] %v");
-
   auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
       log_file_path, 1048576 * 50 /*MB*/, 3);
 
   auto console_sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
 
   spdlog::init_thread_pool(256, 1);
-  auto logger = std::make_shared<spdlog::async_logger>(
+  auto default_logger = std::make_shared<spdlog::async_logger>(
       "default", spdlog::sinks_init_list{file_sink, console_sink},
+      spdlog::thread_pool(), spdlog::async_overflow_policy::block);
+
+  auto taskscheduler_logger = std::make_shared<spdlog::async_logger>(
+      "taskscheduler", spdlog::sinks_init_list{file_sink, console_sink},
+      spdlog::thread_pool(), spdlog::async_overflow_policy::block);
+
+  auto cranedkeeper_logger = std::make_shared<spdlog::async_logger>(
+      "cranedkeeper", spdlog::sinks_init_list{file_sink, console_sink},
       spdlog::thread_pool(), spdlog::async_overflow_policy::block);
 
   file_sink->set_level(level);
   console_sink->set_level(level);
 
-  spdlog::set_default_logger(logger);
+  default_logger->set_pattern("[%Y-%m-%d %H:%M:%S.%f] [%n] [%^%l%$] [%s:%#] %v");
+  taskscheduler_logger->set_pattern("[%Y-%m-%d %H:%M:%S.%f] [%n] [%^%l%$] [%s:%#] %v");
+  cranedkeeper_logger->set_pattern("[%Y-%m-%d %H:%M:%S.%f] [%n] [%^%l%$] [%s:%#] %v");
+
+  spdlog::register_logger(default_logger);
+  spdlog::register_logger(taskscheduler_logger);
+  spdlog::register_logger(cranedkeeper_logger);
 
   spdlog::flush_on(spdlog::level::err);
   spdlog::flush_every(std::chrono::seconds(1));
