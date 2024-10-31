3867ed65e6fbe888675169fb957f758aadf2ab8f
huerni <47264950+huerni@users.noreply.github.com>
2024-10-31 17:11:13 +0800

    feat: jwt util

---
+++ src/Utilities/PublicHeader/jwt.cpp
@@ -0,0 +1,41 @@
+#include "crane/jwt.h"
+
+#include <exception>
+
+#include "jwt-cpp/jwt.h"
+#include "jwt-cpp/traits/kazuho-picojson/defaults.h"
+
+namespace util {
+std::string GenerateToken(
+    const std::string& secret,
+    const std::unordered_map<std::string, std::string>& claims) {
+  auto creater = jwt::create().set_issuer("crane").set_type("JWS");
+  for (const auto& [k, v] : claims) {
+    creater.set_payload_claim(k, jwt::claim(v));
+  }
+  return creater.sign(jwt::algorithm::hs256{secret});
+}
+
+bool VerifyToken(const std::string& secret, const std::string& token) {
+  try {
+    auto decoded = jwt::decode(token);
+    jwt::verify()
+        .allow_algorithm(jwt::algorithm::hs256{secret})
+        .with_issuer("crane")
+        .verify(decoded);
+  } catch (std::invalid_argument& a) {
+    return false;
+  } catch (jwt::error::token_verification_exception& e) {
+    return false;
+  }
+
+  return true;
+}
+
+std::string GetClaim(const std::string& key, const std::string& token) {
+  auto decoded = jwt::decode(token);
+
+  return decoded.get_payload_claim(key).as_string();
+}
+
+}  // namespace util
