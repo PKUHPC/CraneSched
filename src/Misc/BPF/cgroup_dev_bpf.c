986772e1ec535f3e32b3ea1d44013cd0a0ad49e9
wtr <wangtaoran0504nb@outlook.com>
2024-11-07 09:00:02 +0800

    resolve comment

--- src/Misc/BPF/cgroup_dev_bpf.c
+++ src/Misc/BPF/cgroup_dev_bpf.c
@@ -71,37 +71,6 @@ int craned_device_access(struct bpf_cgroup_dev_ctx *ctx) {
                meta->minor, meta->access);
 
   if (ctx->major == meta->major && ctx->minor == meta->minor) {
-    //     if (meta->permission == ALLOW) {
-    //       int flag = 0;
-    //       if (access & BPF_DEVCG_ACC_READ)
-    //         if (meta->access & BPF_DEVCG_ACC_READ) {
-    // #ifdef BPF_DEBUG
-    //           bpf_printk("Read access allowed for device major=%d,
-    //           minor=%d\n",
-    //                      ctx->major, ctx->minor);
-    // #endif
-    //           flag |= 1;
-    //         }
-    //       if (access & BPF_DEVCG_ACC_WRITE)
-    //         if (meta->access & BPF_DEVCG_ACC_WRITE) {
-    // #ifdef BPF_DEBUG
-    //           bpf_printk("Write access allowed for device major=%d,
-    //           minor=%d\n",
-    //                      ctx->major, ctx->minor);
-    // #endif
-    //           flag |= 1;
-    //         }
-    //       if (access & BPF_DEVCG_ACC_MKNOD)
-    //         if (meta->access & BPF_DEVCG_ACC_MKNOD) {
-    // #ifdef BPF_DEBUG
-    //           bpf_printk("Write access allowed for device major=%d,
-    //           minor=%d\n",
-    //                      ctx->major, ctx->minor);
-    // #endif
-    //           flag |= 1;
-    //         }
-    //       return flag;
-    //     } else
     if (meta->permission == DENY) {
       int flag = 1;
       if (access & BPF_DEVCG_ACC_READ)
