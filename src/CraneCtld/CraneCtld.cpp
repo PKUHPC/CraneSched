6b8653b711e1ba2f03f04934bccb4c9b32094a07
huerni <47264950+huerni@users.noreply.github.com>
2024-11-18 15:48:08 +0800

    fix: The backend retrieves the token with control characters present.

--- src/CraneCtld/CraneCtld.cpp
+++ src/CraneCtld/CraneCtld.cpp
@@ -24,8 +24,6 @@
 #include <yaml-cpp/yaml.h>
 
 #include <cxxopts.hpp>
-#include <filesystem>
-#include <memory>
 
 #include "AccountManager.h"
 #include "AccountMetaContainer.h"
@@ -38,11 +36,8 @@
 #include "DbClient.h"
 #include "EmbeddedDbClient.h"
 #include "TaskScheduler.h"
-#include "crane/GrpcHelper.h"
-#include "crane/Logger.h"
 #include "crane/Network.h"
 #include "crane/PluginClient.h"
-#include "crane/PublicHeader.h"
 
 void ParseConfig(int argc, char** argv) {
   cxxopts::Options options("cranectld");
