5bc25bb1fedf11fd30f614975745e1817a7f4a7c
huerni <47264950+huerni@users.noreply.github.com>
2024-12-05 16:38:50 +0800

    refactor

--- src/CraneCtld/TaskScheduler.cpp
+++ src/CraneCtld/TaskScheduler.cpp
@@ -522,7 +522,6 @@ void TaskScheduler::PutRecoveredTaskIntoRunningQueueLock_(
   for (const CranedId& craned_id : task->CranedIds())
     g_meta_container->MallocResourceFromNode(craned_id, task->TaskId(),
                                              task->Resources());
-  g_account_meta_container->MallocQosResourceFromUser(task->Username(), *task);
   // The order of LockGuards matters.
   LockGuard running_guard(&m_running_task_map_mtx_);
   LockGuard indexes_guard(&m_task_indexes_mtx_);
@@ -2483,10 +2482,6 @@ void MinLoadFirst::NodeSelect(
         continue;
       }
 
-      bool apply_qos_result = g_account_meta_container->CheckQosLimitOnUser(
-          task->Username(), *task);
-      if (!apply_qos_result) continue;
-
       // For pending tasks, the `start time` field in TaskInCtld means expected
       // start time and the `end time` is expected end time.
       // For running tasks, the `start time` means the time when it starts and
@@ -2535,8 +2530,6 @@ void MinLoadFirst::NodeSelect(
       for (CranedId const& craned_id : craned_ids)
         g_meta_container->MallocResourceFromNode(craned_id, task->TaskId(),
                                                  task->Resources());
-      g_account_meta_container->MallocQosResourceFromUser(task->Username(),
-                                                          *task);
       std::unique_ptr<TaskInCtld> moved_task;
 
       // Move task out of pending_task_map and insert it to the
