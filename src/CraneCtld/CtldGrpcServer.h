6f9e228fad8deac09a337d472d32bf21923de2ad
huerni <47264950+huerni@users.noreply.github.com>
2024-12-13 14:24:37 +0800

    feat: ModifyPartitionAllowedAccounts grpc

--- src/CraneCtld/CtldGrpcServer.h
+++ src/CraneCtld/CtldGrpcServer.h
@@ -227,6 +227,11 @@ class CraneCtldServiceImpl final : public crane::grpc::CraneCtld::Service {
       const crane::grpc::ModifyCranedStateRequest *request,
       crane::grpc::ModifyCranedStateReply *response) override;
 
+  grpc::Status ModifyPartitionAllowedAccounts(
+      grpc::ServerContext *context,
+      const crane::grpc::ModifyPartitionAllowedAccountsRequest *request,
+      crane::grpc::ModifyPartitionAllowedAccountsReply *response) override;
+
   grpc::Status AddAccount(grpc::ServerContext *context,
                           const crane::grpc::AddAccountRequest *request,
                           crane::grpc::AddAccountReply *response) override;
