043cfc953c52347dfb2a2faa8cd5b9598bbe891c
huerni <47264950+huerni@users.noreply.github.com>
2024-12-26 15:17:29 +0800

    feat: vault config

--- src/CraneCtld/CtldPublicDefs.h
+++ src/CraneCtld/CtldPublicDefs.h
@@ -110,6 +110,15 @@ struct Config {
   };
   CranedListenConf CranedListenConf;
 
+  struct VaultConfig {
+    std::string Addr;
+    std::string Port;
+    std::string Token;
+    std::string DomainSuffix;
+  };
+
+  VaultConfig VaultConf;
+
   struct Priority {
     enum TypeEnum { Basic, MultiFactor };
     TypeEnum Type;
