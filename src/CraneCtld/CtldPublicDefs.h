73f4eb7c890449d1b874d9f55153dc688148cb1d
huerni <47264950+huerni@users.noreply.github.com>
2025-01-06 13:16:43 +0800

    refactor config

--- src/CraneCtld/CtldPublicDefs.h
+++ src/CraneCtld/CtldPublicDefs.h
@@ -113,6 +113,7 @@ struct Config {
     std::string Addr;
     std::string Port;
     std::string Token;
+    bool Tls;
     std::string DomainSuffix;
     std::unordered_set<std::string> AllowedNodes;
 
