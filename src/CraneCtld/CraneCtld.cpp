d23f6b7415b94de301cd45a7c52855d444e9af15
Li Junlin <xiafeng.li@foxmail.com>
2025-04-10 16:27:57 +0800

    refactor: Ctld will not connect craned at start.
    Signed-off-by: Li Junlin <xiafeng.li@foxmail.com>

--- src/CraneCtld/CraneCtld.cpp
+++ src/CraneCtld/CraneCtld.cpp
@@ -813,7 +813,7 @@ void InitializeCtldGlobalVariables() {
   });
 
   g_craned_keeper->SetCranedDisconnectedCb([](const CranedId& craned_id) {
-    CRANE_DEBUG("CranedNode #{} Connected.", craned_id);
+    CRANE_DEBUG("CranedNode #{} Disconnected.", craned_id);
     g_meta_container->CranedDown(craned_id);
     g_task_scheduler->TerminateTasksOnCraned(craned_id,
                                              ExitCode::kExitCodeCranedDown);
@@ -843,24 +843,24 @@ void InitializeCtldGlobalVariables() {
   std::chrono::time_point<std::chrono::system_clock> wait_end_point =
       std::chrono::system_clock::now() + std::chrono::seconds(timeout);
 
-  g_craned_keeper->InitAndRegisterCraneds(to_register_craned_list);
-  while (true) {
-    auto online_cnt = g_craned_keeper->AvailableCranedCount();
-    if (online_cnt >= to_registered_craneds_cnt) {
-      CRANE_INFO("All craned nodes are up.");
-      break;
-    }
-
-    std::this_thread::sleep_for(
-        std::chrono::microseconds(timeout * 1000 /*ms*/ / 100));
-    if (std::chrono::system_clock::now() > wait_end_point) {
-      CRANE_INFO(
-          "Waiting all craned node to be online timed out. Continuing. "
-          "{} craned is online. Total: {}.",
-          online_cnt, to_registered_craneds_cnt);
-      break;
-    }
-  }
+  // g_craned_keeper->InitAndRegisterCraneds(to_register_craned_list);
+  // while (true) {
+  //   auto online_cnt = g_craned_keeper->AvailableCranedCount();
+  //   if (online_cnt >= to_registered_craneds_cnt) {
+  //     CRANE_INFO("All craned nodes are up.");
+  //     break;
+  //   }
+  //
+  //   std::this_thread::sleep_for(
+  //       std::chrono::microseconds(timeout * 1000 /*ms*/ / 100));
+  //   if (std::chrono::system_clock::now() > wait_end_point) {
+  //     CRANE_INFO(
+  //         "Waiting all craned node to be online timed out. Continuing. "
+  //         "{} craned is online. Total: {}.",
+  //         online_cnt, to_registered_craneds_cnt);
+  //     break;
+  //   }
+  // }
 
   g_task_scheduler = std::make_unique<TaskScheduler>();
   ok = g_task_scheduler->Init();
