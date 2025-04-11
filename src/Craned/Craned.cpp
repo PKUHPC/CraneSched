5cbea45c3b8efb129287a777133defaab4a2fbbf
db <1301189887@qq.com>
2025-04-11 11:03:18 +0800

    add crane env

--- src/Craned/Craned.cpp
+++ src/Craned/Craned.cpp
@@ -80,6 +80,8 @@ void ParseConfig(int argc, char** argv) {
   if (std::filesystem::exists(config_path)) {
     try {
       YAML::Node config = YAML::LoadFile(config_path);
+      if (config["ClusterName"])
+        g_config.CraneClusterName = config["ClusterName"].as<std::string>();
 
       if (config["CraneBaseDir"])
         g_config.CraneBaseDir = config["CraneBaseDir"].as<std::string>();
