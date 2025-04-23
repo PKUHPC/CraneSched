e7c24a3ca5098d23895ff621fb8d02ba177380b8
huerni <47264950+huerni@users.noreply.github.com>
2025-04-23 17:23:12 +0800

    feat: fence ring

--- src/Craned/Pmix/ReverseTree.cpp
+++ src/Craned/Pmix/ReverseTree.cpp
@@ -16,10 +16,10 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include "ReverseTree.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "ReverseTree.h"
 
 namespace pmix {
 
