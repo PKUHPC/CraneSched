79d3041cbe5b2bb3bcb5433a10f7e4fb0e7919c7
龙晨涛 <1910960801@qq.com>
2025-04-30 00:53:31 +0800

    Fix throw std::overflow_error("not enough space")

--- src/CraneCtld/CtldGrpcServer.cpp
+++ src/CraneCtld/CtldGrpcServer.cpp
@@ -1282,7 +1282,6 @@ grpc::Status CraneCtldServiceImpl::QueryClusterInfo(
   }
 #endif
   *response = g_meta_container->QueryClusterInfo(*request);
-  //  g_raft_server->get_all_keys();
   return grpc::Status::OK;
 }
 
@@ -1629,13 +1628,13 @@ grpc::Status CraneCtldServiceImpl::AddFollower(
 
   if (!found) {
     response->set_ok(false);
-    response->set_reason("Follower not found in config file");
+    response->set_reason("Follower not found in config file.");
     return grpc::Status::OK;
   }
 
   if (g_raft_server->CheckServerNodeExist(server_id)) {
     response->set_ok(false);
-    response->set_reason("Follower already exists in the Raft cluster");
+    response->set_reason("Follower already exists in the Raft cluster.");
     return grpc::Status::OK;
   }
 
@@ -1681,7 +1680,7 @@ grpc::Status CraneCtldServiceImpl::RemoveFollower(
 
   if (!found) {
     response->set_ok(false);
-    response->set_reason("Follower not found in config file");
+    response->set_reason("Follower not found in config file.");
     return grpc::Status::OK;
   }
 
