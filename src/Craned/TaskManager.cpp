bd6ffc89596467906b2f4fcbcacd3fb215830a18
Li Junlin <xiafeng.li@foxmail.com>
2025-05-09 20:45:35 +0800

    fix
    Signed-off-by: Li Junlin <xiafeng.li@foxmail.com>

--- src/Craned/TaskManager.cpp
+++ src/Craned/TaskManager.cpp
@@ -1039,7 +1039,7 @@ CraneErrCode TaskManager::SpawnProcessInInstance_(TaskInstance* instance,
 }
 
 CraneErrCode TaskManager::ExecuteTaskAsync(crane::grpc::TaskToD const& task) {
-  CRANE_INFO("Executing task #{} {}", task.task_id(), task.DebugString());
+  CRANE_INFO("Executing task #{}.", task.task_id());
 
   auto instance = std::make_unique<TaskInstance>();
 
