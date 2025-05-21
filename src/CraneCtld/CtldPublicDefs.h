38cd1678d72ad028732a4f020851590a03678786
huerni <47264950+huerni@users.noreply.github.com>
2025-05-21 10:07:45 +0800

    feat: task account_chain

--- src/CraneCtld/CtldPublicDefs.h
+++ src/CraneCtld/CtldPublicDefs.h
@@ -706,6 +706,8 @@ struct TaskInCtld {
 
   std::unordered_map<std::string, uint32_t> licenses_count;
 
+  std::list<std::string> account_chain;
+
  private:
   /* ------------- [2] -------------
    * Fields that won't change after this task is accepted.
