fd54597c600d772d498db42270c313dac14a5892
db <1301189887@qq.com>
2025-06-10 14:22:47 +0800

    fix ai comments

--- src/Craned/TaskManager.h
+++ src/Craned/TaskManager.h
@@ -311,10 +311,14 @@ class TaskManager {
     }
     auto signal_handle = m_uvw_loop_->resource<uvw::timer_handle>();
     signal_handle->on<uvw::timer_event>(
-        [this, instance](const uvw::timer_event&, uvw::timer_handle& h) {
-          if (!instance) return;
-          int signal_number = instance->task.signal_param().signal_number();
-          for (const auto& [_, pr_instance] : instance->processes) {
+        [this, task_id = instance->task.task_id()](const uvw::timer_event&,
+                                                   uvw::timer_handle& h) {
+          auto iter = m_task_map_.find(task_id);
+          if (iter == m_task_map_.end()) return;
+          TaskInstance* inst = iter->second.get();
+          if (!inst->task.has_signal_param()) return;
+          int signal_number = inst->task.signal_param().signal_number();
+          for (const auto& [_, pr_instance] : inst->processes) {
             KillProcessInstance_(pr_instance.get(), signal_number);
           }
         });
