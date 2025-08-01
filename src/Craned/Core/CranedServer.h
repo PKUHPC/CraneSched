52cb97816b464b12b2528516045c62e095312a95
db <1301189887@qq.com>
2025-08-01 14:21:21 +0800

    rebase

--- src/Craned/Core/CranedServer.h
+++ src/Craned/Core/CranedServer.h
@@ -57,9 +57,9 @@ class CranedServiceImpl : public Craned::Service {
                          const crane::grpc::ConfigureCranedRequest *request,
                          google::protobuf::Empty *response) override;
 
-  grpc::Status GetConfigHash(::grpc::ServerContext *context,
-                             const ::google::protobuf::Empty *request,
-                             ::crane::grpc::ConfigHashReply *response) override;
+  grpc::Status ConfigHashCalc(
+      ::grpc::ServerContext *context, const ::google::protobuf::Empty *request,
+      ::crane::grpc::ConfigHashCalcReply *response) override;
 
   grpc::Status ExecuteSteps(grpc::ServerContext *context,
                             const crane::grpc::ExecuteStepsRequest *request,
