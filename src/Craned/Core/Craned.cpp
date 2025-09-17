270fa9485778d6a161f4e9bee79de3756d28f36f
huerni <47264950+huerni@users.noreply.github.com>
2025-09-17 10:48:57 +0800

    feat: HealthCheck config read

--- src/Craned/Core/Craned.cpp
+++ src/Craned/Core/Craned.cpp
@@ -648,6 +648,25 @@ void ParseConfig(int argc, char** argv) {
           }
         }
 
+        if (config["HealthCheck"]) {
+          const auto& health_check_config = config["HealthCheck"];
+          g_config.HealthCheck.Program = YamlValueOr(health_check_config["Program"], "");
+          if (g_config.HealthCheck.Program.empty()) {
+            CRANE_ERROR("HealthCheckProgram is not configured");
+            std::exit(1);
+          }
+          g_config.HealthCheck.Interval = YamlValueOr<uint64_t>(health_check_config["Interval"], 0L);
+          g_config.HealthCheck.NodeState = absl::StripAsciiWhitespace(
+            absl::AsciiStrToLower(YamlValueOr(health_check_config["NodeState"], "any")));
+          if (g_config.HealthCheck.NodeState != "any" &&
+              g_config.HealthCheck.NodeState != "idle" &&
+              g_config.HealthCheck.NodeState != "mixed" && g_config.HealthCheck.NodeState != "alloc") {
+            CRANE_WARN("HealthCheckNodeState is not valid, reset to any");
+            g_config.HealthCheck.NodeState = "any";
+          }
+          g_config.HealthCheck.Cycle = YamlValueOr<bool>(health_check_config["Cycle"], false);
+        }
+
         if (config["Plugin"]) {
           const auto& plugin_config = config["Plugin"];
           g_config.Plugin.Enabled =
