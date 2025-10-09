19c42e85d7e39b15209bff37eca134f45f4f62bf
edragain2nd <edragain@163.com>
2025-10-09 22:27:08 +0800

    adjust Cranectld deadline handle

--- src/CraneCtld/TaskScheduler.cpp
+++ src/CraneCtld/TaskScheduler.cpp
@@ -729,6 +729,8 @@ void TaskScheduler::ScheduleThread_() {
   std::chrono::steady_clock::time_point begin;
   std::chrono::steady_clock::time_point end;
 
+  absl::Time absl_begin;
+  std::vector<task_id_t> cancel_tasks;
   while (!m_thread_stop_) {
     // Note: In other parts of code, we must avoid the happening of the
     // situation where m_running_task_map_mtx is acquired and then
@@ -785,6 +787,25 @@ void TaskScheduler::ScheduleThread_() {
       g_meta_container->StartLogging();
 
       begin = std::chrono::steady_clock::now();
+      absl_begin = absl::Now();
+      for (const auto& [task_id, task] : m_pending_task_map_) {
+        if (task->deadline_time < absl_begin) {
+          CRANE_DEBUG("Task #{} was added to cancel_tasks", task_id);
+          cancel_tasks.push_back(task_id);
+        }
+      }
+      for (const auto task_id : cancel_tasks) {
+        auto pd_it = m_pending_task_map_.find(task_id);
+        if (pd_it != m_pending_task_map_.end()) {
+          auto& task = pd_it->second;
+          m_cancel_task_queue_.enqueue(
+              CancelPendingTaskQueueElem{.task = std::move(task)});
+          m_cancel_task_async_handle_->send();
+          m_pending_task_map_.erase(pd_it);
+          CRANE_DEBUG("Terminate task #{} due to deadline", task_id);
+        }
+      }
+      cancel_tasks.clear();
 
       m_node_selection_algo_->NodeSelect(now, running_jobs, pending_jobs);
 
