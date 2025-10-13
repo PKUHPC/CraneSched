112f9dc78dd98ea9de3356d7edac8cc800d68b2f
huerni <47264950+huerni@users.noreply.github.com>
2025-10-13 15:45:55 +0800

    feat: add m_health_check_thread_

--- src/Utilities/PublicHeader/include/crane/PublicHeader.h
+++ src/Utilities/PublicHeader/include/crane/PublicHeader.h
@@ -49,6 +49,8 @@ using CraneExpectedRich = std::expected<T, CraneRichError>;
 constexpr const char* kLogPattern =
     "[%^%L%$ %C-%m-%d %H:%M:%S.%e %s:%#][%n] %v";
 
+constexpr const char* HealthCheckFailedReason = "Node failed health check";
+
 inline const char* const kDefaultHost = "0.0.0.0";
 
 inline const char* kCtldDefaultPort = "10011";
