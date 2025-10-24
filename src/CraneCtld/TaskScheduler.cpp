7cb4ac671d92cc7a777e1aefdcad658b465fe71e
huerni <47264950+huerni@users.noreply.github.com>
2025-10-24 14:28:06 +0800

    refactor

--- src/CraneCtld/TaskScheduler.cpp
+++ src/CraneCtld/TaskScheduler.cpp
@@ -1062,7 +1062,7 @@ void TaskScheduler::ScheduleThread_() {
             alloc_job_latch.count_down();
             return;
           }
-
+          // TODO: is prolog failed?
           auto err = stub->AllocJobs(jobs);
           if (err == CraneErrCode::SUCCESS) {
             alloc_job_latch.count_down();
