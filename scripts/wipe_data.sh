3b8661e6c9bfad8e8341c93e0b79fdcb5a4b15f5
db <1301189887@qq.com>
2025-11-12 16:34:40 +0800

    fix comments

--- scripts/wipe_data.sh
+++ scripts/wipe_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Usage: $0 mode(1:acct_table | 2:qos_table | 3:task_table | 4:user_table | 5:all | 6:acct_table+qos_table+user_table| 7:hour_table+day_table+month_table |8: summary_table)"
+echo "Usage: $0 mode(1:acct_table | 2:qos_table | 3:task_table | 4:user_table | 5:all | 6:acct_table+qos_table+user_table| 7:hour_table+day_table+month_table |8: summary_time_table)"
 
 if [ "$#" -ne 1 ]; then
   echo "Parameter error: please input mode num!"
@@ -69,13 +69,13 @@ fi
 if [ "$mode" -eq 4 ] || [ "$mode" -eq 5 ] || [ "$mode" -eq 6 ]; then
   wipe_collection user_table
 fi
-if [ "$mode" -eq 5 ] || [ "$mode" -eq 7 ] || [ "$mode" -eq 3 ]; then
+if [ "$mode" -eq 5 ] || [ "$mode" -eq 7 ]; then
   wipe_collection hour_job_summary_table
   wipe_collection day_job_summary_table
   wipe_collection month_job_summary_table
 fi
 
-if [ "$mode" -eq 5 ] || [ "$mode" -eq 8 ] || [ "$mode" -eq 3 ]; then
+if [ "$mode" -eq 5 ] || [ "$mode" -eq 8 ]; then
   wipe_collection "summary_time_table"
 fi
 
