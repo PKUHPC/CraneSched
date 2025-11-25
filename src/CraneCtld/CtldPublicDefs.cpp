b47fa9f509ec4e3f4c845c750b5fa6ee9c99173e
db <1301189887@qq.com>
2025-11-25 15:26:54 +0800

    add gpus

--- src/CraneCtld/CtldPublicDefs.cpp
+++ src/CraneCtld/CtldPublicDefs.cpp
@@ -419,10 +419,7 @@ crane::grpc::StepToD DaemonStepInCtld::GetStepToD(
   step_to_d.set_cwd(this->job->cwd);
   step_to_d.set_ntasks_per_node(this->job->ntasks_per_node);
   step_to_d.set_cpus_per_task(this->job->TaskToCtld().cpus_per_task());
-
-  // for (const auto& hostname : this->job->excluded_nodes)
-  //   step_to_d.mutable_exclude_nodelist()->Add()->assign(hostname);
-
+  step_to_d.set_total_gpus(this->requested_node_res_view.GpuCount());
   return step_to_d;
 }
 
