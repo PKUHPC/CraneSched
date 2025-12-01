3293ff485cb7fbbb0fdc16ef05db091ec457ba81
db <1301189887@qq.com>
2025-12-01 15:46:02 +0800

    del valid_wckey

--- src/CraneCtld/CtldPublicDefs.h
+++ src/CraneCtld/CtldPublicDefs.h
@@ -197,7 +197,6 @@ struct Config {
   bool JobFileOpenModeAppend{false};
   bool IgnoreConfigInconsistency{false};
   bool MustNeedWckey{false};
-  bool WckeyValid{false};
 };
 
 struct RunTimeStatus {
