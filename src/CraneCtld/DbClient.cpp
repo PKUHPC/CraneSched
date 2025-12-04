e2b88c3f2f950f05dfb6a7f5f54a09f6ea4a4986
db <1301189887@qq.com>
2025-12-04 11:38:44 +0800

    rebase

--- src/CraneCtld/DbClient.cpp
+++ src/CraneCtld/DbClient.cpp
@@ -1576,11 +1576,11 @@ MongodbClient::document MongodbClient::TaskInEmbeddedDbToDocument_(
   // 20 script        state          timelimit     time_submit work_dir
   // 25 submit_line   exit_code      username       qos        get_user_env
   // 30 type          extra_attr     reservation   exclusive   cpus_alloc
-  // 35 mem_alloc     device_map     container     has_job_info  licenses_alloc
+  // 35 mem_alloc     device_map     meta_container  has_job_info licenses_alloc
   // 40 nodename_list
 
   // clang-format off
-  std::array<std::string, 40> fields{
+  std::array<std::string, 41> fields{
     // 0 - 4
     "task_id",  "task_db_id", "mod_time",    "deleted",  "account",
     // 5 - 9
@@ -1611,7 +1611,7 @@ MongodbClient::document MongodbClient::TaskInEmbeddedDbToDocument_(
              int32_t, std::string, std::string, bool, double,        /*30-34*/
              int64_t, DeviceMap, std::optional<ContainerMetaInTask>, /*35-37*/
              bool, std::unordered_map<std::string, uint32_t>,        /*38-39*/
-            bsoncxx::array::value>                                   /*40*/
+             bsoncxx::array::value>                                  /*40-44*/
       values{                                                        // 0-4
              static_cast<int32_t>(runtime_attr.task_id()),
              runtime_attr.task_db_id(), absl::ToUnixSeconds(absl::Now()), false,
@@ -1650,7 +1650,8 @@ MongodbClient::document MongodbClient::TaskInEmbeddedDbToDocument_(
              std::unordered_map<std::string, uint32_t>{
                  runtime_attr.actual_licenses().begin(),
                  runtime_attr.actual_licenses().end()},
-                 bsoncxx::array::value{nodename_list_array.view()};
+             // 40-44
+             bsoncxx::array::value{nodename_list_array.view()}};
 
   return DocumentConstructor_(fields, values);
 }
@@ -1685,11 +1686,11 @@ MongodbClient::document MongodbClient::TaskInCtldToDocument_(TaskInCtld* task) {
   // 20 script        state          timelimit     time_submit work_dir
   // 25 submit_line   exit_code      username       qos        get_user_env
   // 30 type          extra_attr     reservation    exclusive  cpus_alloc
-  // 35 mem_alloc     device_map     meta_container      has_job_info licenses_alloc
+  // 35 mem_alloc     device_map     meta_container  has_job_info licenses_alloc
   // 40 nodename_list
 
   // clang-format off
-  std::array<std::string, 40> fields{
+  std::array<std::string, 41> fields{
       // 0 - 4
       "task_id",  "task_db_id", "mod_time",    "deleted",  "account",
       // 5 - 9
@@ -1720,7 +1721,7 @@ MongodbClient::document MongodbClient::TaskInCtldToDocument_(TaskInCtld* task) {
              int32_t, std::string, std::string, bool, double,        /*30-34*/
              int64_t, DeviceMap, std::optional<ContainerMetaInTask>, /*35-37*/
              bool, std::unordered_map<std::string, uint32_t>,        /*38-39*/
-             bsoncxx::array::value>                                   /*40*/
+             bsoncxx::array::value>                                  /*40-44*/
       values{                                                        // 0-4
              static_cast<int32_t>(task->TaskId()), task->TaskDbId(),
              absl::ToUnixSeconds(absl::Now()), false, task->account,
@@ -1748,9 +1749,10 @@ MongodbClient::document MongodbClient::TaskInCtldToDocument_(TaskInCtld* task) {
              static_cast<int64_t>(task->allocated_res_view.MemoryBytes()),
              task->allocated_res_view.GetDeviceMap(), container_meta,
              true /* Mark the document having complete job info */,
-             task->licenses_count, 
+             task->licenses_count,
              // 40-44
-            bsoncxx::array::value{nodename_list_array.view()};
+             bsoncxx::array::value{nodename_list_array.view()}};
+
   return DocumentConstructor_(fields, values);
 }
 
