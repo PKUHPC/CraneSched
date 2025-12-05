881ce9c1aab6c07c09587cc94ad9a023679a80d7
huerni <47264950+huerni@users.noreply.github.com>
2025-12-05 13:49:41 +0800

    feat: refactor config

--- src/CraneCtld/CtldPublicDefs.h
+++ src/CraneCtld/CtldPublicDefs.h
@@ -161,7 +161,7 @@ struct Config {
   };
   ContainerConfig Container;
 
-  struct JobLogHookConfig {
+  struct JobLifecycleHookConfig {
     std::vector<std::string> ProLogs;  // ctld prologs
     std::vector<std::string> EpiLogs;  // ctld epilogs
     uint32_t PrologTimeout{0};
@@ -169,7 +169,7 @@ struct Config {
     uint32_t PrologEpilogTimeout{0};
   };
 
-  JobLogHookConfig JobLogHook;
+  JobLifecycleHookConfig JobLifecycleHook;
 
   bool CompressedRpc{};
 
