df3a38d2e327020fed1141fc25b26f95fe5027da
1daidai1 <166272788+1daidai1@users.noreply.github.com>
2025-12-30 18:44:26 +0800

    fix: proto index (#600)
    Signed-off-by: junlinli <xiafeng.li@foxmail.com>
    
    fix comments
    
    del exclude
    
    change log
    
    add log
    
    rebase
    
    Reapply "feat: Add job env (#495)" (#573)
    
    This reverts commit 268d2602bdb7b0fa8665748036b493f5d5bb3edf.
    
    add env

--- src/Utilities/PublicHeader/include/crane/PublicHeader.h
+++ src/Utilities/PublicHeader/include/crane/PublicHeader.h
@@ -105,6 +105,8 @@ inline const char* const kDefaultSupervisorUnixSockDir = "/tmp/crane";
 
 inline const char* const kDefaultPlugindUnixSockPath = "cplugind/cplugind.sock";
 
+inline const char* const kResourceTypeGpu = "gpu";
+
 constexpr uint64_t kTaskMinTimeLimitSec = 11;
 constexpr int64_t kTaskMaxTimeLimitSec =
     google::protobuf::util::TimeUtil::kDurationMaxSeconds;
@@ -528,6 +530,7 @@ class ResourceView {
 
   double CpuCount() const;
   uint64_t MemoryBytes() const;
+  uint64_t GpuCount() const;
 
   AllocatableResource& GetAllocatableRes() { return allocatable_res; }
   const AllocatableResource& GetAllocatableRes() const {
