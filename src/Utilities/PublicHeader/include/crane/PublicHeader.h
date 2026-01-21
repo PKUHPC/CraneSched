b175e6a014d8a19988df482de16e1dab73b4b51b
huerni <47264950+huerni@users.noreply.github.com>
2026-01-21 16:13:15 +0800

    update kCraneErrStrArr

--- src/Utilities/PublicHeader/include/crane/PublicHeader.h
+++ src/Utilities/PublicHeader/include/crane/PublicHeader.h
@@ -285,6 +285,7 @@ constexpr std::array<std::string_view, crane::grpc::ErrCode_ARRAYSIZE>
         "ERR_INVALID_ARGUMENT",
         "ERR_RESOURCE_ALREADY_EXIST"
         "The current submitted job exceeds the QoS limit (MaxSubmitJobsPerAccount)"
+        "ERR_USER_HAS_TASK"
     };
 // clang-format on
 }  // namespace Internal
