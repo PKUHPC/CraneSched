22b8e5c6d849bd0ad1cfa7381872dfe9f20cada4
huerni <1547156325@qq.com>
2026-03-12 09:57:05 +0800

    fix: flags

--- src/CraneCtld/CtldPublicDefs.h
+++ src/CraneCtld/CtldPublicDefs.h
@@ -1128,7 +1128,7 @@ struct TaskInCtld {
   void SetFieldsOfTaskInfo(crane::grpc::TaskInfo* task_info);
 };
 
-enum class QosFlags { None, DenyOnLimit, _Count };
+enum class QosFlags { DenyOnLimit, _Count };
 
 struct Qos {
   bool deleted = false;
