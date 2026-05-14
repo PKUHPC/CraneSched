71ed490a86d125538dc0c7aa3f6523d9fb969c96
Junlin Li <70465472+L-Xiafeng@users.noreply.github.com>
2026-05-14 10:06:12 +0800

    Log supervisor execution details in StepInstance
    Add logging for supervisor execution in StepInstance.

--- src/Craned/Core/StepInstance.cpp
+++ src/Craned/Core/StepInstance.cpp
@@ -458,7 +458,10 @@ CraneErrCode StepInstance::SpawnSupervisor(const EnvMap& job_env_map) {
     for (auto& arg : string_argv) {
       argv.push_back(arg.c_str());
     }
-    argv.push_back(nullptr);
+    argv.push_back(nullptr); // argv must be null-terminated.
+          fmt::print(stderr,
+               "[{:%Y-%m-%d %H:%M:%S}] [Step #{}.{}]: Executing supervisor\n",
+               std::chrono::system_clock::now(), job_id, step_id);
 
     // Use execvp to search the kSupervisorPath in the PATH.
     execvp(g_config.Supervisor.Path.c_str(),
