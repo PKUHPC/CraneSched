63eb429a7c5bd8872624640ffe7857996faefea1
junlinli <xiafeng.li@foxmail.com>
2026-08-14 17:25:19 +0800

    docs: tolerate missing pam_crane module

--- scripts/uninstall_craned.sh
+++ scripts/uninstall_craned.sh
@@ -9,7 +9,6 @@ rm -f /etc/systemd/system/craned.service
 
 rm -f /usr/lib64/security/pam_crane.so
 if grep -q "pam_crane.so" /etc/pam.d/sshd; then
-   sed -i '/account    required     pam_crane.so/d' /etc/pam.d/sshd
-   sed -i '/session    required     pam_crane.so/d' /etc/pam.d/sshd
+   sed -i '/pam_crane\.so/d' /etc/pam.d/sshd
 fi
 systemctl daemon-reload
